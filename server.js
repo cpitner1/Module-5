fbe74ce64bb5e7fbd3d566e76b1a3a15878d65ba
cpitner1 <cpitner1@ivytech.edu>
2022-09-17 12:03:04 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+
+    // set header content type
+    response.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(request.url) {
+        case '/':
+            path += 'index.html';
+            response.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            response.statusCode = 200;
+            break;
+        case '/about-me':
+            response.statusCode = 301;
+            response.setHeader('Location', '/about');
+            response.end();
+            break;
+        default:
+            path += '404.html';
+            response.statusCode = 404;
+            break;
+    }
+
+    // send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            response.end();
+        } else {
+            // response.write(data);
+            response.end(data);
+        }
+    })
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
+});
